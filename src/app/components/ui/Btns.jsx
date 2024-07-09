f2197df0acc8d8ddd425ac12b579567c32a08a62
Juan Miguel Spolzino Mendoza <juan.spolisino@gmail.com>
2024-07-09 11:38:00 -0400

    Btn Whatsapp

--- src/app/components/ui/Btns.jsx
+++ src/app/components/ui/Btns.jsx
@@ -6,13 +6,13 @@ export const Wsp = () => {
   return (
     <Link
       href="https://api.whatsapp.com/send?phone=+12143840158&text=Hola!%20quiero%20de%20hacer%20un%20pedido."
-      className="bg-green-600 hover:bg-green-700 transition-colors duration-300 ease-in-out text-white p-3 rounded-full shadow-lg flex items-center justify-center z-50 px-7 font-bold"
+      className="bg-green-600 hover:bg-green-700 transition-colors duration-300 ease-in-out text-white rounded-full shadow-lg flex items-center justify-center z-50 px-10 py-4 font-bold text-center"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="WhatsApp"
     >
       PEDIDOS CLIC AQUI
-      <FaWhatsapp className="text-2xl ps-2" />
+      <FaWhatsapp className="text-5xl ps-2" />
     </Link>
   );
 };
